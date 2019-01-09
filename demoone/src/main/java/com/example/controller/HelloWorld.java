372ff48771140cd1ad4cd685bf3bbee66f1c1d29
taidibao <46479855+taidibao@users.noreply.github.com>
2019-01-09 19:10:43 +0800

    Update HelloWorld.java

--- demoone/src/main/java/com/example/controller/HelloWorld.java
+++ demoone/src/main/java/com/example/controller/HelloWorld.java
@@ -11,9 +11,7 @@ public class HelloWorld {
         System.out.println("test1...");
     }
 
-    public void test2() {
-        System.out.println("test2...");
-    }
+
 
     public void test3() {
         System.out.println("test3...");
