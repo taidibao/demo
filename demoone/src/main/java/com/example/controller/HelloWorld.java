61f009a6d2cd99dc9203030f46ed9ece6c08f8fb
taidibao <859209493@qq.com>
2019-01-09 18:58:07 +0800

    本地新增接口文件测试

--- demoone/src/main/java/com/example/controller/HelloWorld.java
+++ demoone/src/main/java/com/example/controller/HelloWorld.java
@@ -10,7 +10,12 @@ public class HelloWorld {
     public void test1() {
         System.out.println("test1...");
     }
-  public void test2() {
-    System.out.println("test2...");
-  }
+
+    public void test2() {
+        System.out.println("test2...");
+    }
+
+    public void test3() {
+        System.out.println("test3...");
+    }
 }
